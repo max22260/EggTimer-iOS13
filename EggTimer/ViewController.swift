b6d371801f8b9b51a9ae80b326188cc38a4626c4
Ahmed Nagy <ahmed.nagy.radwan@gmail.com>
2021-01-19 16:05:19 +0200

    create progressBar !!

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTime = ["Soft" : 3 , "Medium" : 420 , "Hard" : 720]
+    var totalTime = 0
+    var timePassed = 0
+    var timer = Timer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        totalTime = eggTime[sender.currentTitle!]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(ViewController.updateCounter),
+                                     userInfo: nil,
+                                     repeats: true
+        )
+        
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if totalTime > timePassed {
+            
+        
+            timePassed += 1
+            print("totalTime\(totalTime)")
+            print("timePass\(timePassed)")
 
-
+          let  percentageProgress = Float(timePassed) / Float(totalTime)
+            print(percentageProgress)
+            progressBar.progress = percentageProgress
+            
+            
+            
+        }else{
+            
+            timer.invalidate()
+            titleLabel.text = "Done ! "
+        }
+    }
 }
